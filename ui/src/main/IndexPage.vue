16d585f6a069da5dcb4388d16627c9b15e632fa4
unknown <mharfemicaroz@gmail.com>
2023-08-10 11:37:24 +0800

    fix error route

--- ui/src/main/IndexPage.vue
+++ ui/src/main/IndexPage.vue
@@ -1,52 +1,52 @@
-<template >
-    <GlobalLoader v-if="isLoading"/>
-    <div class="main-wrapper">
-        <HeaderComponent />
-        <SideBar />
-        <div class="page-wrapper">
-            <div class="content">
-                <router-view :key="$route.path"></router-view>
-            </div>
-        </div>
+<template>
+  <GlobalLoader v-if="isLoading" />
+  <div class="main-wrapper">
+    <HeaderComponent />
+    <SideBar />
+    <div class="page-wrapper">
+      <div class="content">
+        <router-view :key="$route.path"></router-view>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import GlobalLoader from '../common/GlobalLoader.vue';
-import HeaderComponent from '../common/HeaderComponent2.vue';
-import SideBar from '../common/SideBar.vue';
+import GlobalLoader from "../common/GlobalLoader.vue";
+import HeaderComponent from "../common/HeaderComponent2.vue";
+import SideBar from "../common/SideBar.vue";
 
 export default {
-    components: {
-        GlobalLoader,
-        HeaderComponent,
-        SideBar,
-    },
-    data() {
-        return {
-            isLoading: true, // Initially set to true to show loading state
-        };
-    },
-    watch: {
-        // Watch for route changes
-        '$route': {
-            immediate: true, // Run the handler immediately
-            handler() {
-                // Use nextTick to wait until the next DOM update cycle
-                this.$nextTick(() => {
-                    this.isLoading = true;
-                    
-                    // Set a timeout of 1000 milliseconds (1 second)
-                    setTimeout(() => {
-                        this.isLoading = false;
-                    }, 1000);
-                });
-            },
-        },
-    },
-    created() {
-        this.$router.push({ path: localStorage.getItem('savedPath'), query: { t: Date.now() } });
-    },
-}
+  components: {
+    GlobalLoader,
+    HeaderComponent,
+    SideBar,
+  },
+  data() {
+    return {
+      isLoading: true, // Initially set to true to show loading state
+    };
+  },
+  watch: {
+    // Watch for route changes
+    $route: {
+      immediate: true, // Run the handler immediately
+      handler() {
+        // Use nextTick to wait until the next DOM update cycle
+        this.$nextTick(() => {
+          this.isLoading = true;
 
+          // Set a timeout of 1000 milliseconds (1 second)
+          setTimeout(() => {
+            this.isLoading = false;
+          }, 1000);
+        });
+      },
+    },
+  },
+  created() {},
+  mounted() {
+    this.$router.push({ path: localStorage.getItem("savedPath") });
+  },
+};
 </script>
 <style></style>
