984f6123e5046e7a1fc3ae3b875e61f20ef04b7d
unknown <mharfemicaroz@gmail.com>
2023-09-25 22:37:40 +0800

    change port 8082

--- ui/src/main.js
+++ ui/src/main.js
@@ -8,7 +8,7 @@ import "sweetalert2/dist/sweetalert2.min.css";
 
 const app = createApp(App);
 app.config.globalProperties = {
-  API_URL: `http://${window.location.hostname}:8081/api/`,
+  API_URL: `http://${window.location.hostname}:8082/api/`,
 };
 
 app.use(router);
